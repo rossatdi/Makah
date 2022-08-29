daad90c4004ed013c216b93c7aeb4f5e732ea373
TimGibbs <timgibbs1988@gmail.com>
2022-08-29 08:07:24 +0100

    hide weapon filter in faction view

--- src/pages/factions/FactonView.tsx
+++ src/pages/factions/FactonView.tsx
@@ -62,7 +62,7 @@ const FactionView = ({ faction }: {faction: Faction; }) => {
         <SkillTile key={o.name} skill={o} type="equipment" />
       ))}
       <h3>Weapons</h3>
-      {WeaponBlock(faction.weapons, faction.name)}
+      {WeaponBlock(faction.weapons, faction.name, true)}
     </div>
   );
 };
