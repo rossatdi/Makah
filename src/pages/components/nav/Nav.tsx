db253afd81966a347751918ef49bfd6a791edb16
TimGibbs <timgibbs1988@gmail.com>
2022-08-29 07:33:17 +0100

    updatign routing

--- src/pages/components/nav/Nav.tsx
+++ src/pages/components/nav/Nav.tsx
@@ -6,34 +6,34 @@ import { NavLink  } from "react-router-dom";
 const NavView = () => {
   const factionRoutes = Factions.map((o,i)=>(
     <NavDropdown.Item key={i}>
-      <Nav.Link as={NavLink } to={`/Makah/factions/${o.slug}`}>{o.name}</Nav.Link>
+      <Nav.Link as={NavLink } to={`/factions/${o.slug}`}>{o.name}</Nav.Link>
     </NavDropdown.Item>
   ))
 
  return (
 <Nav>
-    <Nav.Link as={NavLink } to="/Makah/">Home</Nav.Link>
-    <Nav.Link as={NavLink } to="/Makah/actions">Actions</Nav.Link>
+    <Nav.Link as={NavLink } to="/">Home</Nav.Link>
+    <Nav.Link as={NavLink } to="/actions">Actions</Nav.Link>
     <NavDropdown title="Weapons">
       <NavDropdown.Item>
-        <Nav.Link as={NavLink } to="/Makah/weapons/all">All</Nav.Link>
+        <Nav.Link as={NavLink } to="/weapons/all">All</Nav.Link>
       </NavDropdown.Item>
       <NavDropdown.Item>
-        <Nav.Link as={NavLink } to="/Makah/weapons/generic">Generic</Nav.Link>
+        <Nav.Link as={NavLink } to="/weapons/generic">Generic</Nav.Link>
       </NavDropdown.Item>
       <NavDropdown.Item>
-        <Nav.Link as={NavLink } to="/Makah/weapons/imperial">Imperial</Nav.Link>
+        <Nav.Link as={NavLink } to="/weapons/imperial">Imperial</Nav.Link>
       </NavDropdown.Item>
       <NavDropdown.Item>
-        <Nav.Link as={NavLink } to="/Makah/weapons/rules">Special Rules</Nav.Link>
+        <Nav.Link as={NavLink } to="/weapons/rules">Special Rules</Nav.Link>
       </NavDropdown.Item>
     </NavDropdown>
-    <Nav.Link as={NavLink } to="/Makah/skills">Skills</Nav.Link>
+    <Nav.Link as={NavLink } to="/skills">Skills</Nav.Link>
     <NavDropdown title="Factions">
       {factionRoutes}
     </NavDropdown>
-    <Nav.Link as={NavLink } to="/Makah/glossary">Glossary</Nav.Link>
-    <Nav.Link as={NavLink } to="/Makah/crew">Crew</Nav.Link>
+    <Nav.Link as={NavLink } to="/glossary">Glossary</Nav.Link>
+    <Nav.Link as={NavLink } to="/crew">Crew</Nav.Link>
     </Nav>)}
 
     export default NavView;
