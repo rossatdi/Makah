e061ac10ddc5ee3b7309a3d3784a768a30328a1d
TimGibbs <timgibbs1988@gmail.com>
2022-08-29 10:00:07 +0100

    tables

--- src/pages/skills/Skills.tsx
+++ src/pages/skills/Skills.tsx
@@ -1,21 +1,34 @@
+import { Table } from "react-bootstrap";
 import { Skills } from "../../data/Skills";
 
-const skillList = Skills.map(skill=><tr><td>{skill.name}</td><td>{skill.effect}</td><td>{skill.pt}</td></tr>)
+const skillList = Skills.map((skill) => (
+  <tr>
+    <td>{skill.name}</td>
+    <td>{skill.effect}</td>
+    <td>{skill.pt}</td>
+  </tr>
+));
 
-const page = () => <div>
-        <h1>SKILLS</h1>
-        <p>The following list of generic skills can be purchased during crew building or added during campaigns. Unless otherwise
-stated, skills can be purchased by any faction. You may wish to interpret the skills as a piece of equipment or skill that’s
-more relevant to your faction.</p>
-        <table>
-            <tr>
-                <th>Skill</th>
-                <th>Effect</th>
-                <th>Points</th>
-            </tr>
-            {skillList}
-        </table>
-    </div>
-
+const page = () => (
+  <div>
+    <h1>SKILLS</h1>
+    <p>
+      The following list of generic skills can be purchased during crew building
+      or added during campaigns. Unless otherwise stated, skills can be
+      purchased by any faction. You may wish to interpret the skills as a piece
+      of equipment or skill that’s more relevant to your faction.
+    </p>
+    <Table striped>
+      <thead>
+        <tr>
+          <th>Skill</th>
+          <th>Effect</th>
+          <th>Points</th>
+        </tr>
+      </thead>
+      <tbody>{skillList}</tbody>
+    </Table>
+  </div>
+);
 
 export default page;
