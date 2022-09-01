d7f2d012ce4251f60a5a99c4486efc54b21d488e
tim <me@tgibbs.uk>
2022-09-01 16:00:53 +0100

    keys are stupid

--- src/pages/components/weaponTIle/WeaponTile.tsx
+++ src/pages/components/weaponTIle/WeaponTile.tsx
@@ -2,6 +2,8 @@ import './WeaponTile.css'
 import Weapon from '../../../types/Weapon'
 import { Card, Col, Container, Row } from 'react-bootstrap'
 import GlossaryOverlay from '../GlossaryOverlay'
+import { getKey } from '../../../functions/keys'
+import React from 'react'
 
 export interface WeaponTileProps 
 {
@@ -12,35 +14,35 @@ export interface WeaponTileProps
 }
 
 const WeaponTile = ({weapon, type, background, faction}:WeaponTileProps) => {
-    return (<Card className={`tile`}>
-                <Card.Header>
+    return (<Card className={`tile`} key={getKey()}>
+                <Card.Header key={getKey()} >
                     <div className="tileHeader">
                         <span className="title">{weapon.name}</span>
                         {weapon.pt!== undefined && weapon.pt!=="-" && <span className="pt">{`${weapon.pt}pt`}</span>}
                     </div>
                 </Card.Header>
-                <Card.Body className="body" style={{backgroundImage: background ?`url(${background})`:"none"}} >
-                    <Container>
+                <Card.Body key={getKey()}  className="body" style={{backgroundImage: background ?`url(${background})`:"none"}} >
+                    <Container key={getKey()}>
                     {weapon.profiles.map(o=>
-                    <>
-                        <Row>
+                    <React.Fragment key={getKey()}>
+                        <Row key={getKey()}>
                             <Col>Type</Col>
                             <Col>Atk</Col>
                             <Col>Dam</Col>
                             <Col>AP</Col>
                         </Row>
-                        <Row>
-                            <Col>{o.types.map((p,j)=>GlossaryOverlay({ skill:p, item:<p key={j}>{p.name}</p>}))}</Col>
+                        <Row key={getKey()}>
+                            <Col>{o.types.map((p,j)=>GlossaryOverlay({ skill:p, item:<p key={getKey()} >{p.name}</p>}))}</Col>
                             <Col>{o.attack}</Col>
                             <Col>{o.dam}</Col>
                             <Col>{o.ap}</Col>
                         </Row>
-                        <Row>
-                            {o.special.sort((a,b)=>a.name.localeCompare(b.name)).map((p,j)=>GlossaryOverlay({ skill:p, item:<Col key={j}>{p.name}</Col>}))}
+                        <Row key={getKey()}>
+                            {o.special.sort((a,b)=>a.name.localeCompare(b.name)).map((p,j)=>GlossaryOverlay({ skill:p, item:<Col key={getKey()} >{p.name}</Col>}))}
                         </Row>
-                        </>
+                        </React.Fragment>
                         )}
-                        {weapon.notes && <Row>{weapon.notes}</Row>}
+                        {weapon.notes && <Row key={getKey()}>{weapon.notes}</Row>}
                     </Container>
                 </Card.Body>
     </Card>)
