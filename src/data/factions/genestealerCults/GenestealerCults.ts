764de242f44ab3fa96141bf63de83e95237ebe4f
tim <me@tgibbs.uk>
2022-09-02 14:35:09 +0100

    added imperial weapons to gsc

--- src/data/factions/genestealerCults/GenestealerCults.ts
+++ src/data/factions/genestealerCults/GenestealerCults.ts
@@ -8,6 +8,7 @@ import background from '../../../Images/factionIcons/GenestealerCults.svg'
 import GenestealerCultEquipment from "./GenestealerCultEquipment";
 import GenestealerCultOperatives from "./GenestealerCultOperatives";
 import GenestealerCultsWeapons from "./GenestealerCultWeapons";
+import ImperialWeaponsList from "../../weapons/ImperialWeapons";
 const Name : string = "Genestealer Cults"
 
 export const SeizeTheInitiative:FactionAbility =
@@ -25,7 +26,7 @@ export const GenestealerCults : Faction =
     quote:"\"In the sunless body of our Hive, the flock of the faithful grows. We are guided by our Great Father's sacred vision, protected by his loving hands. Within these holy shadows we dwell, while the impure bathe in spire-top starlight. And what lies between those stars? What sails beyond those distant suns? Shrouded in night, borne within that eternal black: Our redeemers, our Gods -- truer than the silent, long-dead Emperor -- the Children of the Void. We hear them in our dreams, we feel them in our blood. They call and we will answer. We will rise and claim the engines of labour that have bound us in servitude. This world will burn. It will be cleansed, purified, made ready, and the Heavens will deliver our reward!\"",
     attribution:"Unknown Genestealer Cult Magus",
     operativeTypes:GenestealerCultOperatives,
-    weapons:[...GenestealerCultsWeapons, ...GenericWeaponsList].filter(onlyUnique),
+    weapons:[...GenestealerCultsWeapons, ...ImperialWeaponsList, ...GenericWeaponsList].filter(onlyUnique),
     equipment:GenestealerCultEquipment,
     ability:SeizeTheInitiative
 }
