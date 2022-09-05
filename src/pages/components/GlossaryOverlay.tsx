9e3810a25e2de72b31e3bf44c68b64d1484a4c1e
tim <me@tgibbs.uk>
2022-09-05 14:11:02 +0100

    apply glossary overlay

--- src/pages/components/GlossaryOverlay.tsx
+++ src/pages/components/GlossaryOverlay.tsx
@@ -1,6 +1,24 @@
 import { OverlayTrigger, Popover } from "react-bootstrap"
+import GlossaryTerms from "../../data/GlossaryTerms";
 import { getKey } from "../../functions/keys";
 import IGlossaryItem from "../../interfaces/IGlossaryItem"
+
+const mapOverlay = (value: string): JSX.Element =>{
+    for (const term of GlossaryTerms){
+        if(value === term.name){
+            return <GlossaryOverlay skill={term} item={<span>{value}</span>} />
+        }
+    }
+    return <>{value}</>
+}
+
+export const applyGlossaryOverlay = (text: string) : JSX.Element =>{
+    const split = text.split(' ')
+        .map(o=>mapOverlay(o))
+        .reduce((a,b)=> <>{a} {b}</>)
+    return <>{split}</>
+}
+
 
 const popover = (rule:IGlossaryItem) =>
 (<Popover>
@@ -9,9 +27,9 @@ const popover = (rule:IGlossaryItem) =>
 </Popover>
 )
 
-const GlossaryOverlay = (props :{skill:IGlossaryItem; item:JSX.Element;}) =>(
-    <OverlayTrigger key={getKey()}  trigger={["hover","focus"]} delay={{show:250, hide:400}} placement="bottom" overlay={popover(props.skill)}>
-        {props.item}
+const GlossaryOverlay = ({skill,item} :{skill:IGlossaryItem; item:JSX.Element;}) =>(
+    <OverlayTrigger key={getKey()}  trigger={["hover","focus"]} delay={{show:250, hide:400}} placement="bottom" overlay={popover(skill)}>
+        {item}
     </OverlayTrigger>
 )
 
